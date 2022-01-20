2e71d3213b73b0a2ea503e2c5e2a115e2cb1a080
PolarianDev <polariandev@protonmail.com>
2022-01-20 14:33:53 +0000

    plugin base

--- src/main/java/com/github/crafttogether/hydroperms/HydroPerms.java
+++ src/main/java/com/github/crafttogether/hydroperms/HydroPerms.java
@@ -1,16 +1,33 @@
 package com.github.crafttogether.hydroperms;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class HydroPerms extends JavaPlugin {
 
+    JavaPlugin plugin;
+
     @Override
     public void onEnable() {
-
+        plugin = this;
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        try {
+            getConfig().load(Files.newBufferedReader(Path.of(getDataFolder() + "/config.yml")));
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "HydroPerms loaded");
     }
 
     @Override
     public void onDisable() {
-
+        Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "HydroPerms disabled");
     }
 }
